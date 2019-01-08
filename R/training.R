704fa6cd1cf7134d52ed0a1424ece5a5985a4b1a
Bruno Marques <bruno@bmarques.net>
2019-01-07 23:07:47 -0200

    Added hyperparameter optimization

--- R/training.R
+++ R/training.R
@@ -10,6 +10,7 @@ run.benchmark <- function(data, seed = 8080) {
     mlr::makeClassifTask("women.league.normalized", mlr::normalizeFeatures(input.wl), "result"),
     mlr::makeClassifTask("world.cup.normalized", mlr::normalizeFeatures(input.wc), "result")
   )
+  pred.tuner <- mlr::makeTuneControlRandom()
   pred.learners <- list(
     # support feature importance OOB
     mlr::makeLearner("classif.boosting", predict.type = "prob"),
@@ -28,10 +29,21 @@ run.benchmark <- function(data, seed = 8080) {
     mlr::makeLearner("classif.nnet", predict.type = "prob"),
     mlr::makeLearner("classif.svm", predict.type = "prob")
   )
+  pred.learners.tuned <- lapply(pred.learners, function(l) {
+    par.set <- tryCatch(mlrHyperopt::getParConfigParSet(mlrHyperopt::getDefaultParConfig(l)),
+      error = function(e) NULL
+    )
+    if (!is.null(par.set)) {
+      mlr::makeTuneWrapper(l, mlr::hout, par.set = par.set, control = pred.tuner)
+    }
+  }) %>% purrr::compact()
   pred.measures <- list(
     mlr::kappa, mlr::logloss, mlr::acc, mlr::multiclass.au1p, mlr::multiclass.au1u,
     mlr::multiclass.aunp, mlr::multiclass.aunu, mlr::multiclass.brier, multiclass.mcc
   )
 
-  mlr::benchmark(learners = pred.learners, tasks = pred.tasks, resamplings = mlr::hout, measures = pred.measures, show.info = FALSE)
+  mlr::benchmark(
+    learners = c(pred.learners, pred.learners.tuned), tasks = pred.tasks,
+    resamplings = mlr::hout, measures = pred.measures, show.info = FALSE
+  )
 }
